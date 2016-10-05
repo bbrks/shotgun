bc9e6a9f46d991a401996cf6672d58c2242f8381
Ben Brooks <ben@bbrks.me>
2016-10-05 22:50:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# shotgun
+
+Take the shotgun approach to your terminal.
+
+A tool for running commands in parallel on a set of directories.
+
+## Installation
+
+```
+go get -u github.com/bbrks/shotgun
+```
+
+## Usage
+
+```
+Shotgun is a tool for running commands in parallel on a set of directories.
+
+Usage:
+  shotgun [options] command
+
+Options:
+  -d string
+    	Working directory - search from here (default ".")
+  -dry-run
+    	Print what would be run where, without actually doing it
+  -f string
+    	Filter directories by RE2 regexp (default ".*")
+
+Examples:
+  shotgun git pull
+    	Run a command in each child directory of the current
+  shotgun -f '^a.*$' git pull
+    	Run a command in each directory beginning with the letter 'a'
+  shotgun -dir $GOPATH/src/github.com/bbrks git status --short
+    	Run a command for each directory in '$GOPATH/src/github.com/bbrks'
+  shotgun 'git checkout -- .; git checkout develop; git fetch; git pull'
+    	Wrap commands in quotes and separate by semicolons to chain sequentially
+  shotgun -dry-run 'rm LICENSE.md'
+    	Print what would be run where
+```
---
+++ main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"regexp"
+	"strings"
+	"sync"
+)
+
+const version = "0.1"
+
+var (
+	f      = flag.String("f", ".*", "Filter directories by RE2 regexp")
+	dir    = flag.String("d", ".", "Working directory - search from here")
+	dryRun = flag.Bool("dry-run", false, "Print what would be run where, without actually doing it")
+	ver    = flag.Bool("version", false, "Print version and exit")
+)
+
+func main() {
+	flag.Parse()
+
+	if *ver {
+		fmt.Printf("shotgun version %s\n", version)
+		os.Exit(0)
+	}
+
+	// no arguments passed or invalid directory filter
+	fRegexp, err := regexp.Compile(*f)
+	if len(flag.Args()) < 1 || err != nil {
+		usage()
+		os.Exit(1)
+	}
+
+	// invalid working directory
+	f, err := ioutil.ReadDir(*dir)
+	if err != nil {
+		panic(err)
+	}
+
+	var (
+		cmd = strings.Join(flag.Args(), " ")
+		wg  = sync.WaitGroup{}
+	)
+
+	for _, d := range f {
+		wg.Add(1)
+		go func(d os.FileInfo) {
+			defer wg.Done()
+			if d.IsDir() && fRegexp.MatchString(d.Name()) {
+				if *dryRun {
+					fmt.Printf("Would run '%s' in '%s'\n", cmd, d.Name())
+					return
+				}
+				exec.Command("sh", "-c", "cd "+d.Name()+";"+cmd).Run()
+				fmt.Printf("Running '%s' in '%s'\n", cmd, d.Name())
+			}
+		}(d)
+	}
+	wg.Wait()
+}
+
+var usage = func() {
+	fmt.Fprintf(os.Stderr, "Shotgun is a tool for running commands in parallel on a set of directories.\n")
+	fmt.Fprintf(os.Stderr, "\nUsage:\n")
+	fmt.Fprintf(os.Stderr, "  shotgun [options] command\n")
+	fmt.Fprintf(os.Stderr, "\nOptions:\n")
+	flag.PrintDefaults()
+	fmt.Fprintf(os.Stderr, "\nExamples:\n")
+	fmt.Fprintf(os.Stderr, "  %s\n    \t%s\n",
+		"shotgun git pull",
+		"Run a command in each child directory of the current")
+	fmt.Fprintf(os.Stderr, "  %s\n    \t%s\n",
+		"shotgun -f '^a.*$' git pull",
+		"Run a command in each directory beginning with the letter 'a'")
+	fmt.Fprintf(os.Stderr, "  %s\n    \t%s\n",
+		"shotgun -dir $GOPATH/src/github.com/bbrks git status --short",
+		"Run a command for each directory in '$GOPATH/src/github.com/bbrks'")
+	fmt.Fprintf(os.Stderr, "  %s\n    \t%s\n",
+		"shotgun 'git checkout -- .; git checkout develop; git fetch; git pull'",
+		"Wrap commands in quotes and separate by semicolons to chain sequentially")
+	fmt.Fprintf(os.Stderr, "  %s\n    \t%s\n",
+		"shotgun -dry-run 'rm LICENSE.md'",
+		"Print what would be run where")
+
+}
